a753482c480978f20825111666cf10fedea83dc9
Patrik Suhaj <suhajpato@gmail.com>
2023-11-19 22:34:49 +0100

    added express

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const http = require('http');
-const server = http.createServer();
+const express = require('express');
+
+// express application
+const app = express();
+app.use((req, res, next) => {
+    console.log('1st');
+    // next will call another .use() below this function
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('2nd in the middleware');
+    res.send('<h1>fucker</h1>');
+});
+
+const server = http.createServer(app);
 server.listen(8000);
