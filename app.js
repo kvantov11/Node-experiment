21deb29982a48040b63a6f8dde8d6b7833ff5c24
Patrik Suhaj <suhajpato@gmail.com>
2023-11-20 00:10:29 +0100

    adding filter (segment) admin

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const shopRoutes = require('./routes/shop');
 // express application
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use((req, res) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(8000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 router.post('/product', (req, res) => {
