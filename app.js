da16937ac8dfa95843cdfc4c259a179628c4b306
Patrik Suhaj <suhajpato@gmail.com>
2023-11-20 00:03:29 +0100

    changing to admin and shop

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 // express application
 const app = express();
-
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use('/add-product', (req, res) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/', (req, res, next) => {
-    console.log('wtf');
-    res.send('<h1>omg</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(8000);
