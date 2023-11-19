4e79f6a318b69baf05d5c020e1f4e99209948269
Patrik Suhaj <suhajpato@gmail.com>
2023-11-20 00:38:17 +0100

    adding html files

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>omg</h1>');
+    // absolute path to project directory
+    // dont use / the join function will do it without that properly
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
 module.exports = router;
