02aeee18fedb3bf4c9867a65c6afe20422f0290b
Patrik Suhaj <suhajpato@gmail.com>
2023-11-19 22:40:20 +0100

    replaced http by express

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const http = require('http');
 const express = require('express');
-
 // express application
 const app = express();
+
 app.use((req, res, next) => {
     console.log('1st');
     // next will call another .use() below this function
@@ -14,5 +13,4 @@ app.use((req, res, next) => {
     res.send('<h1>fucker</h1>');
 });
 
-const server = http.createServer(app);
-server.listen(8000);
+app.listen(8000);
